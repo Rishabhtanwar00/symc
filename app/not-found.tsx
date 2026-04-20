619c354783bb586a53ed9a5f3b13111782e2c49e
rishabhlivenewshour <rishabh@livenewshour.com>
2026-04-20 17:15:27 +0530

    removed unused code (#13)

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -1,6 +1,6 @@
 const NotFound = () => {
   return (
-    <div className="h-[500px] w-full flex items-center justify-center text-[clamp(1.6rem,3.5vw,2.8rem)] text-gold font-cinzel-deco">
+    <div className="h-[500px] w-full flex items-center justify-center text-[clamp(1.6rem,3.5vw,2.8rem)] text-gold font-playfair">
       Coming soon....
     </div>
   );
